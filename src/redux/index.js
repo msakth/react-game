96424490be34ca851af1480eb05e4fc518d377f3
Muthu <muthu@muthus-mbp.mynet>
2020-09-11 22:04:10 +0100

    add convenient index file for redux

---
+++ src/redux/index.js
@@ -0,0 +1,20 @@
+import {
+  fetchPlayers,
+  fetchSuccess,
+  fetchFailure,
+} from "./actionCreators/playersActionCreator";
+
+import {
+  shufflePlayers,
+  shuffleSuccess,
+  shuffleFailure,
+} from "./actionCreators/shuffleActionCreator";
+
+export {
+  fetchPlayers,
+  fetchSuccess,
+  fetchFailure,
+  shufflePlayers,
+  shuffleSuccess,
+  shuffleFailure,
+};
