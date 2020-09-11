df886417ce1c4d5c2da624ee669c0d9a446d5636
Muthu <muthu@muthus-mbp.mynet>
2020-09-11 20:49:58 +0100

    add config file to hold player json url

---
+++ src/config.js
@@ -0,0 +1,8 @@
+const PlayerConfig = {
+  sourceUrl:
+    process.env.NODE_ENV === "production"
+      ? "https://gist.githubusercontent.com/liamjdouglas/bb40ee8721f1a9313c22c6ea0851a105/raw/6b6fc89d55ebe4d9b05c1469349af33651d7e7f1/Player.json"
+      : "https://gist.githubusercontent.com/liamjdouglas/bb40ee8721f1a9313c22c6ea0851a105/raw/6b6fc89d55ebe4d9b05c1469349af33651d7e7f1/Player.json",
+};
+
+export default PlayerConfig;
